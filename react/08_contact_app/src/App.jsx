ed9b57db593241399f75d1f69b699138d11491f4
Erno Hentonen <erno.hentonen@opiframe.com>
2023-12-19 11:01:18 +0200

    contact app

--- react/08_contact_app/src/App.jsx
+++ react/08_contact_app/src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState,useEffect} from 'react'
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const [state,setState] = useState({
+		list:[]
+	})
+	
+	const [urlRequest,setUrlRequest] = useState({
+		url:"",
+		request:{},
+		action:""
+	})
+
+	//USEEFFECT
+	
+	useEffect(() => {},[urlRequest])
+
+	//HELPER FUNCTIONS
+	
+	const getList = () => {
+		setUrlRequest({
+			url:"/api/contact",
+			request:{
+				"method":"GET"
+			},
+			action:"getlist"
+		})
+	}
+
+	const addContact = (contact) => {
+		setUrlRequest({
+			url:"/api/contact",
+			request:{
+				"method":"POST",
+				"headers":{
+					"Content-Type":"application/json"
+				},
+				"body":JSON.stringify(contact)
+			},
+			action:"add"
+		})
+	}
+	
+	const removeContact = (id) => {
+		setUrlRequest({
+			url:"/api/contact/"+id,
+			request:{
+				"method":"DELETE"
+			},
+			action:"remove"
+		})
+	}
+	
+	const editContact = (contact) => {
+		setUrlRequest({
+			url:"/api/contact/"+contact.id,
+			request:{
+				"method":"PUT",
+				"headers":{
+					"Content-Type":"application/json"
+				},
+				"body":JSON.stringify(contact)
+			},
+			action:"edit"
+		}) 
+	}
+
+	return (
+		<>
+
+		</>
+	)
 }
 
 export default App
