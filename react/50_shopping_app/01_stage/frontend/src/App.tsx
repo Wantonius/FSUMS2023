57ca0ed869e55895afa603ed036c26fad73c40fd
Erno Hentonen <erno.hentonen@opiframe.com>
2024-01-29 14:40:46 +0200

    Shopping app frontend

--- react/50_shopping_app/01_stage/frontend/src/App.tsx
+++ react/50_shopping_app/01_stage/frontend/src/App.tsx
@@ -1,13 +1,21 @@
-
+import {useEffect} from 'react';
+import useAction from './hooks/useAction';
+import ShoppingForm from './components/ShoppingForm';
 
 
 function App() {
 
-  return (
-    <>
+	const action = useAction();
+	
+	useEffect(() => {
+		action.getList();
+	},[])
 
-    </>
-  )
+	return (
+		<>
+			<ShoppingForm add={action.add}/>
+		</>
+	)
 }
 
 export default App
