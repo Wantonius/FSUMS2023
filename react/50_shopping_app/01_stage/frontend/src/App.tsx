e4b87c5f894a8a9fd5842a1b892b5f9b8a0fa31c
Erno Hentonen <erno.hentonen@opiframe.com>
2024-01-30 12:16:02 +0200

    shoppinglist

--- react/50_shopping_app/01_stage/frontend/src/App.tsx
+++ react/50_shopping_app/01_stage/frontend/src/App.tsx
@@ -2,6 +2,8 @@ import {useEffect} from 'react';
 import useAction from './hooks/useAction';
 import ShoppingForm from './components/ShoppingForm';
 import ShoppingList from './components/ShoppingList';
+import Navbar from './components/Navbar';
+import {Routes,Route,Navigate} from 'react-router-dom';
 
 
 function App() {
@@ -14,8 +16,12 @@ function App() {
 
 	return (
 		<>
-			<ShoppingForm add={action.add}/>
-			<ShoppingList list={action.state.list} remove={action.remove} edit={action.edit}/>
+			<Navbar/>
+			<Routes>
+				<Route path="/" element={<ShoppingList list={action.state.list} remove={action.remove} edit={action.edit}/>}/>
+				<Route path="/form" element={<ShoppingForm add={action.add}/>}/>
+				<Route path="*" element={<Navigate to="/"/>}/>
+			</Routes>
 		</>
 	)
 }
