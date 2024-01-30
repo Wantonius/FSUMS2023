5338d11a365ecd24e1c9e25812c2fd1af511ab40
Erno Hentonen <erno.hentonen@opiframe.com>
2024-01-30 13:24:32 +0200

    shoppinglist

--- react/50_shopping_app/02_stage_login/backend/models/item.js
+++ react/50_shopping_app/02_stage_login/backend/models/item.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 let Schema = mongoose.Schema({
 	type:String,
 	count:Number,
-	price:Number
+	price:Number,
+	user:{type:String,index:true}
 })
 
 module.exports = mongoose.model("Item",Schema);
--- react/50_shopping_app/02_stage_login/backend/server.js
+++ react/50_shopping_app/02_stage_login/backend/server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const shoppingRoute = require("./routes/shoppingroute");
+const bcrypt = require("bcrypt");
+const crypto = require("crypto");
+const sessionModel = require("./models/session");
+const userModel = require("./models/user");
 
 let app = express();
 
@@ -16,6 +20,43 @@ mongoose.connect(url).then(
 	() => console.log("Connected to Mongo Atlas"),
 	(err) => console.log("Failed to connect to Mongo Atlas. Reason:",err)
 )
+
+//MIDDLEWARES
+
+const time_to_live_diff = 3600000
+
+//LOGIN API
+
+app.post("/register",function(req,res) {
+	if(!req.body) {
+		return res.status(400).json({"Message":"Bad request"});
+	}
+	if(!req.body.username || !req.body.password) {
+		return res.status(400).json({"Message":"Bad request"});
+	}
+	if(req.body.username.length < 4 || req.body.password.length < 8) {
+		return res.status(400).json({"Message":"Bad request"});
+	}
+	bcrypt.hash(req.body.password,14,function(err,hash) {
+		if(err) {
+			console.log(err);
+			return res.status(500).json({"Message":"Internal server error"})
+		}
+		let user = new userModel({
+			"username":req.body.username,
+			"password":hash
+		})
+		user.save().then(function() {
+			return res.status(200).json({"Message":"Register success"})
+		}).catch(function(err) {
+			if(err.code === 11000) {
+				return res.status(409).json({"Message":"Username already in use"})
+			}
+			console.log(err);
+			return res.status(500).json({"Message":"Internal server error"})
+		})
+	})
+})
 
 let port = process.env.PORT || 3000;
 
