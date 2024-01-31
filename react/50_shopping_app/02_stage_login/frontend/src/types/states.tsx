b973d8d4c664b45a5c4a166cb22f7f8235d24b05
Erno Hentonen <erno.hentonen@opiframe.com>
2024-01-31 09:10:50 +0200

    shoppingapp 2

--- react/50_shopping_app/02_stage_login/frontend/src/types/states.tsx
+++ react/50_shopping_app/02_stage_login/frontend/src/types/states.tsx
@@ -2,4 +2,9 @@ import ShoppingItem from '../models/ShoppingItem';
 
 export interface AppState {
 	list:ShoppingItem[];
+	isLogged:boolean;
+	loading:boolean;
+	error:string;
+	token:string;
+	user:string;
 }
