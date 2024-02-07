7646001bfb9ba568d2ba8c58703dbcdb0b378856
Erno Hentonen <erno.hentonen@opiframe.com>
2024-02-07 13:04:00 +0200

    shopping app redux

--- react/50_shopping_app/03_stage_redux/frontend/src/main.tsx
+++ react/50_shopping_app/03_stage_redux/frontend/src/main.tsx
@@ -2,11 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import {BrowserRouter} from 'react-router-dom';
+import {Store,applyMiddleware,combineReducers,createStore} from 'redux';
+import loginReducer from './reducers/loginReducer';
+import shoppingReducer from './reducers/shoppingReducer';
+import {AppState,ShoppingAction} from './types/states';
+import {Provider} from 'react-redux';
+import {thunk} from 'redux-thunk'
+
+const rootReducer = combineReducers<AppState>({
+	login:loginReducer,
+	shopping:shoppingReducer
+})
+
+const store:Store<AppState,ShoppingAction> = createStore(rootReducer,applyMiddleware(thunk))
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
   <BrowserRouter>
+  <Provider store={store}>
     <App />
+  </Provider>
   </BrowserRouter>
   </React.StrictMode>,
 )
---
+++ react/50_shopping_app/03_stage_redux/frontend/src/reducers/shoppingReducer.tsx
@@ -0,0 +1,79 @@
+import * as actionConstants from '../types/actionConstants';
+import {ShoppingState,ShoppingAction} from '../types/states';
+import {Reducer} from 'redux';
+import ShoppingItem from '../models/ShoppingItem';
+
+const getInitialState = ():ShoppingState => {
+	let state = sessionStorage.getItem("shoppingstate");
+	if(state) {
+		return JSON.parse(state)
+	} else {
+		return {
+			list:[],
+			error:""
+		}
+	}
+}
+
+const initialState = getInitialState();
+
+const saveToStorage = (state:ShoppingState) => {
+	sessionStorage.setItem("shoppingstate",JSON.stringify(state));
+}
+
+const shoppingReducer:Reducer<ShoppingState,ShoppingAction> = (state = initialState,action) => {
+	console.log("ShoppingReducer, action",action);
+	let tempState:ShoppingState = {
+		...state
+	}
+	let error = "";
+	switch(action.type) {
+		case actionConstants.LOADING:
+			tempState = {
+				...state,
+				error:""
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case actionConstants.FETCH_LIST_SUCCESS:
+			let list:ShoppingItem[] = [];
+			if(action.payload) {
+				list = action.payload as ShoppingItem[]
+			}
+			tempState = {
+				...state,
+				list:list
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case actionConstants.ADD_ITEM_SUCCESS:
+		case actionConstants.REMOVE_ITEM_SUCCESS:
+		case actionConstants.EDIT_ITEM_SUCCESS:
+			return state;
+		case actionConstants.FETCH_LIST_FAILED:
+		case actionConstants.ADD_ITEM_FAILED:
+		case actionConstants.REMOVE_ITEM_FAILED:
+		case actionConstants.EDIT_ITEM_FAILED:
+			if(action.payload) {
+				error = action.payload as string;
+			}
+			tempState = {
+				...state,
+				error:error
+			}
+			saveToStorage(tempState);
+			return tempState;
+		case actionConstants.LOGOUT_SUCCESS:
+		case actionConstants.LOGOUT_FAILED:
+			tempState = {
+				list:[],
+				error:""
+			}
+			saveToStorage(tempState);
+			return tempState;
+		default:
+			return state;
+	}
+}	
+
+export default shoppingReducer;
