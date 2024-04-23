8500756740ca0a97f11666d14b660947c9b0a22f
Erno Hentonen <erno.hentonen@opiframe.com>
2024-04-23 13:38:15 +0300

    shopping app

--- react_native/07_shopping_app_with_backend/hooks/useAction.js
+++ react_native/07_shopping_app_with_backend/hooks/useAction.js
@@ -21,7 +21,162 @@ const useAction = () => {
 	
 	//FETCH USEEFFECT
 	
-	useEffect(() => {},[urlRequest]);
+	useEffect(() => {
+		
+		if(!urlRequest.url) {
+			return;
+		}
+		
+		const fetchData = async () => {
+			setState((state) => {
+				return {
+					...state,
+					loading:true,
+					error:""
+				}
+			})
+			let url = baseUrl+urlRequest.url;
+			const response = await fetch(url,urlRequest.request);
+			setState((state) => {
+				return {
+					...state,
+					loading:false
+				}
+			})
+			if(!response) {
+				setState({
+					list:[],
+					isLogged:false,
+					loading:false,
+					token:"",
+					error:"Server never responded. Logging you out."
+				})
+				return;
+			}
+			if(response.ok) {
+				switch(urlRequest.action) {
+					case "register":
+						setState((state) => {
+							return {
+								...state,
+								error:"Register success"
+							}
+						})
+						return;
+					case "login":
+						const data = await response.json();
+						if(!data) {
+							setState((state) => {
+								return {
+									...state,
+									error:"Could not parse login information"
+								}
+							})
+							return;
+						}
+						setState((state) => {
+							return {
+								...state,
+								isLogged:true,
+								token:data.token
+							}
+						})
+						getList(data.token);
+						return;
+					case "logout":
+						setState({
+							list:[],
+							isLogged:false,
+							loading:false,
+							error:"",
+							token:""
+						})
+						return;
+					case "additem":
+					case "removeitem":
+						getList();
+						return;
+					case "getlist":
+						const list = await response.json();
+						if(!list) {
+							setState((state) => {
+								return {
+									...state,
+									error:"Failed to parse shopping information"
+								}
+							})
+							return;
+						}
+						setState((state) => {
+							return {
+								...state,
+								list:list
+							}
+						})
+						return;
+					default:
+						return;
+				}
+			} else {
+				if(response.status === 403) {
+					setState({
+						list:[],
+						isLogged:false,
+						loading:false,
+						token:"",
+						error:"Session expired. Logging you out."
+					})
+					return;
+				}
+				let errorMessage = "Server responded with a status "+response.status+" "+response.statusText
+				switch(urlRequest.action) {
+					case "register":
+						if(response.status === 409) {
+							setState((state) => {
+								return {
+									...state,
+									error:"Username already in use"
+								}
+							})
+						} else {
+							setState((state) => {
+								return {
+									...state,
+									error:errorMessage
+								}
+							})
+						}
+						return;
+					case "logout":
+						setState({
+							list:[],
+							isLogged:false,
+							loading:false,
+							token:"",
+							error:""
+						})
+						return;
+					case "login":
+					case "additem":
+					case "removeitem":
+					case "getlist":
+						setState((state) => {
+							return {
+								...state,
+								error:errorMessage
+							}
+						})
+						return;
+					default:
+						return;
+				}
+				
+			}
+		}
+		
+		fetchData();
+		
+	},[urlRequest]);
 	
 	//LOGIN API
 	
