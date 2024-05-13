55b3329ffba0edffff4eca3b6e7ddfb1b8b3eedf
Erno Hentonen <erno.hentonen@opiframe.com>
2024-05-13 09:50:13 +0300

    rust

---
+++ rust/b_hello_cargo/target/debug/b_hello_cargo.d
@@ -0,0 +1,1 @@
+C:\gits\FSUMS2023\rust\b_hello_cargo\target\debug\b_hello_cargo.exe: C:\gits\FSUMS2023\rust\b_hello_cargo\src\main.rs
---
+++ rust/b_hello_cargo/target/debug/deps/b_hello_cargo.d
@@ -0,0 +1,5 @@
+C:\gits\FSUMS2023\rust\b_hello_cargo\target\debug\deps\b_hello_cargo.exe: src\main.rs
+
+C:\gits\FSUMS2023\rust\b_hello_cargo\target\debug\deps\b_hello_cargo.d: src\main.rs
+
+src\main.rs:
